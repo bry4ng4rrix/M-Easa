65fdee8210ef1cc7be3c11a52187cf098a0dfcaf
bry4ng4rrix <bryanmfb4@gmail.com>
2026-03-13 22:23:33 +0300

    a

--- components/recruiter/company-profile-tab.tsx
+++ components/recruiter/company-profile-tab.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Upload, Save } from 'lucide-react';
 import { FieldGroup, FieldLabel } from '@/components/ui/field';
 
