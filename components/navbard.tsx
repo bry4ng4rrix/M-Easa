aeea4e0d7d42a5ca3630f1d39cee877eac863a80
bry4ng4rrix <bryanmfb4@gmail.com>
2026-03-13 16:09:52 +0300

    navbar

--- components/navbard.tsx
+++ components/navbard.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { User2 , Pickaxe} from "lucide-react"
 
 const Navbard = () => {
   return (
-    <div>
-    
+    <header className='container mx-auto p-4 items-center justify-between flex'>
+      <div>
+        logo
+      </div>
+          
+
+
+    <div className="space-x-2">
+      <Button variant="secondary" size="lg" className="hover:scale-105 "><Pickaxe /> <Link  href="/">Offre d'emplois</Link></Button>
+       <Button size="lg" className="hover:scale-105 hover:bg-violet-900 hover:shadow-xl transition-all duration-300"><User2/> <Link href="/login" > Se connecter</Link></Button>
     </div>
+    </header>
   )
 }
 
