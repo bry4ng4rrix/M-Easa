f1ab9f19a12cf94d7432abc8618db9d1c14e5842
bry4ng4rrix <bryanmfb4@gmail.com>
2026-03-17 10:09:01 +0300

    egoherogheromghearùoigaeroiqh

--- app/page.tsx
+++ app/page.tsx
@@ -241,6 +241,7 @@ export default function HomePage() {
                           >
                             {competence}
                           </Badge>
+                          // comantera
                         ))}
                       </div>
                       <Dialog
